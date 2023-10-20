e1580ea813c53166736ff562942325b48b7a20f6
lazerpope <kalininmi555@gmail.com>
2023-10-20 03:47:01 +0300

    font size switch

---
+++ scripts/fontSizeSwither.js
@@ -0,0 +1,37 @@
+let fontSwitch = document.querySelector('.menu .font-switch')
+
+let fontList = [
+    {
+        size: '0.8rem',
+        comment: 'Мало'
+    },
+    {
+        size: '1rem',
+        comment: 'стандарт'
+    },
+    {
+        size: '1.1rem',
+        comment: 'больше'
+    },
+    {
+        size: '1.2rem',
+        comment: 'много'
+    },
+    {
+        size: '1.3rem',
+        comment: 'ого'
+    },
+]
+
+let fontPointer = 0
+
+fontSwitch.addEventListener('click', nextFontSize)
+
+nextFontSize() 
+
+function nextFontSize() {    
+        fontPointer++
+        fontPointer = fontPointer % fontList.length
+        document.body.style.setProperty('--my-font-adaptive-size', fontList[fontPointer].size);
+        fontSwitch.innerHTML = 'размер шрифта: ' + fontList[fontPointer].comment    
+}
